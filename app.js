3860864ea26c0915ab3599e998d056310179865a
Dawid Kaleta <dawid.kaleta@outlook.com>
2022-11-09 12:23:38 +0100

    Make combining method directly in class

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ const testUrl = 'https://lensdump.com/a/RP3iZ'
 const webScraper = new WebScraper()
 
 async function start(albumUrl) {
-  // TODO: Make combining method directly in class
-  const body = await webScraper.getContent(albumUrl) // Getting the HTML from the album-URL
-  const imageUrls = await webScraper.getImagesFromAlbum(body) // Getting the image-URLs from the album-HTML
-
+  const imageUrls = await webScraper.getImagesFromAlbumUrl(albumUrl)
   console.log(imageUrls)
 }
 
